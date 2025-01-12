d3b92f0e4e9ef0e7b7454b1e9ff0c1a337af24d5
Søren Bruus Zeppelin <soerenbf@gmail.com>
2025-01-12 21:46:08 +0100

    Parse the response from the endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 use clap::Parser;
-use concordium_rust_sdk::id::types::AccountAddress;
+use concordium_rust_sdk::{common::types::Amount, id::types::AccountAddress};
+use serde::{Deserialize, Deserializer};
+use chrono::{DateTime, Utc};
+
+const URL: &str = "https://wallet-proxy.mainnet.concordium.software";
 
 #[derive(Parser, Debug)]
 #[clap(name = "AccountAddressList")]
@@ -8,7 +12,62 @@ struct Args {
     accounts: Vec<AccountAddress>,
 }
 
-fn main() {
+#[derive(Deserialize, Debug)]
+#[serde(tag = "type", rename_all = "camelCase")]
+enum Details {
+    Transfer {
+        #[serde(rename = "transferSource")]
+        from: AccountAddress,
+        #[serde(rename = "transferDestination")]
+        to: AccountAddress,
+        #[serde(rename = "transferAmount")]
+        amount: Amount,
+    },
+    PaydayAccountReward {},
+    ConfigureDelegation {},
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+struct Transaction {
+    #[serde(deserialize_with = "deserialize_block_time")]
+    block_time: DateTime<Utc>,
+    details: Details,
+    cost: Option<Amount>,
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+struct TransactionsResponse {
+    count: u16,
+    limit: u16,
+    transactions: Vec<Transaction>,
+}
+
+fn deserialize_block_time<'de, D>(deserializer: D) -> Result<DateTime<Utc>, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let timestamp = f64::deserialize(deserializer)?;
+    let time: DateTime<Utc> = chrono::DateTime::from_timestamp_millis((timestamp * 1000.0) as i64).expect("Can convert timestamp");
+    Ok(time)
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
     let args = Args::parse();
-    println!("{:?}", args);
+
+    for account in &args.accounts {
+        let res: TransactionsResponse =
+            reqwest::get(format!("{URL}/v1/accTransactions/{}?limit=1000", account))
+                .await?
+                .json()
+                .await?;
+
+        println!("count {}", res.count);
+    }
+
+    println!("success");
+
+    Ok(())
 }
